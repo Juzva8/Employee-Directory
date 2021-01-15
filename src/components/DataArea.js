ee0402e9c168eb70c3fdc824e258e5a9b043e0cc
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-01-14 19:40:26 -0800

    added dscending and ascending

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -13,8 +13,8 @@ order:'descend',
 filteredUsers: [{}]
 };
 headings = [
+{name: 'Image', width:'10%'},
 {name: 'Name', width:'20%'},
-{name: 'Image', width:'20%'},
 {name: 'Phone', width:'20%'},
 {name: 'Email', width:'20%'},
 {name: 'Date of Birth', width:'20%'},
@@ -31,6 +31,44 @@ this.setState({
     
     })
 }
+
+handleSort = heading => { 
+if (this.state.order === 'descend'){
+    this.setState({order: 'ascend'})
+}else {this.setState({order: 'descend'})}
+const compareFnc = (a, b) => {
+    if (this.state.order === "ascend") {
+      // account for missing values
+      if (a[heading] === undefined) {
+        return 1;
+      } else if (b[heading] === undefined) {
+        return -1;
+      }
+      // numerically
+      else if (heading === "name") {
+        return a[heading].first.localeCompare(b[heading].first);
+      } else {
+        return a[heading] - b[heading];
+      }
+    } else {
+      // account for missing values
+      if (a[heading] === undefined) {
+        return 1;
+      } else if (b[heading] === undefined) {
+        return -1;
+      }
+      // numerically
+      else if (heading === "name") {
+        return b[heading].first.localeCompare(a[heading].first);
+      } else {
+        return b[heading] - a[heading];
+      }
+    }
+  }
+const userSort = this.state.filteredUsers.sort(compareFnc)
+this.setState({filteredUsers: userSort});
+}
+
 
 handleSearchChange = event => {
 console.log(event.target.value)
@@ -54,6 +92,7 @@ render() {
          < DataTable 
          headings = {this.headings}
          users = {this.state.filteredUsers}
+         handleSort = {this.handleSort}
          />
          
          </>
